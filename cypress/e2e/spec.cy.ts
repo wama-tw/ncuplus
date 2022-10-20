e51997690dedb76cae177a7da4e2edc1f0cd9e34
wama <36957874+april1026@users.noreply.github.com>
2022-10-20 18:45:09 +0800

    feat: add cypress e2e test(click NCU+, click login, cover background image change)

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,23 @@
-describe("empty spec", () => {
-  it("passes", () => {
-    cy.visit("https://example.cypress.io");
+describe("home page", () => {
+  it("click NCU+", () => {
+    cy.visit("/");
+    cy.contains("NCU+").click();
+
+    cy.url().should("eq", `${Cypress.config().baseUrl}/`);
+  });
+
+  it("click 登入", () => {
+    cy.visit("/");
+    cy.contains("登入").click();
+
+    cy.url().should("contain", "/login");
+  });
+
+  it("cover background image change", () => {
+    cy.visit("/");
+    const prev = cy.get(".bg-cover").invoke("attr", "style");
+    cy.wait(10000);
+
+    expect(cy.get(".bg-cover").invoke("attr", "style")).to.not.eq(prev);
   });
 });
